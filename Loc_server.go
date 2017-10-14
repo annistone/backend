122ae3c3ba0f4e3618ec9be3560d551376e33c29
Ann Korepanova <annistonelivesp@gmail.com>
2017-10-14 17:16:25 +0000

    young server

---
+++ Loc_server.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Link struct {
+	Id   int
+	Name string
+	Type int
+	Link string
+}
+
+type Location struct {
+	Id  int
+	Lat float32
+	Lng float32
+}
+
+type Sight struct {
+	Id       int
+	Name     string
+	Location Location
+	Links    []Link
+}
+
+func sendData(w http.ResponseWriter, r *http.Request) {
+	palace := Sight{
+		Id:   1,
+		Name: "Reds",
+		Location: Location{
+			Id:  1,
+			Lat: 0.23,
+			Lng: 9.3,
+		},
+		Links: []Link{
+			{
+				Id:   1,
+				Name: "palace",
+				Type: 4,
+				Link: "palace.org",
+			},
+			{
+				Id:   1,
+				Name: "palace",
+				Type: 4,
+				Link: "palace.org",
+			},
+		},
+	}
+
+	b, err := json.Marshal(palace)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+
+	fmt.Fprintf(w, string(b))
+}
+
+func main() {
+	http.HandleFunc("/getData", sendData)
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
+
+}
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: Loc_server
