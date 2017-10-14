bba1762f4cc8c163c87c2f7eb510cd7e3d40099f
Ann Korepanova <annistonelivesp@gmail.com>
2017-10-14 21:25:23 +0000

    added images

--- Loc_server.go
+++ Loc_server.go
@@ -9,40 +9,41 @@ import (
 )
 
 type Link struct {
-	Id   int
-	Name string
-	Type int
-	Link string
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+	Type int    `json:"type"`
+	Link string `json:"link"`
 }
 
 type Location struct {
-	Id  int
-	Lat float32
-	Lng float32
+	Id  int     `json:"id"`
+	Lat float32 `json:"lat"`
+	Lng float32 `json:"lng"`
 }
 
 type Sight struct {
-	Id          int
-	Name        string
-	Description string
-	Location    Location
-	Links       []Link
+	Id          int      `json:"id"`
+	Name        string   `json:"name"`
+	Description string   `json:"description"`
+	Image       string   `json:"image"`
+	Location    Location `json:"location"`
+	Links       []Link   `json:"links"`
 }
 
 type Achievement struct {
-	Id          int
-	Text        string
-	Description string
-	Image       string
+	Id          int    `json:"id"`
+	Text        string `json:"text"`
+	Description string `json:"description"`
+	Image       string `json:"image"`
 }
 
 type User struct {
-	Id           int
-	Name         string
-	Last_name    string
-	Rating       int
-	Image        string
-	Achievements []Achievement
+	Id           int           `json:"id"`
+	Name         string        `json:"name"`
+	Last_name    string        `json:"last_name"`
+	Rating       int           `json:"rating"`
+	Image        string        `json:"image"`
+	Achievements []Achievement `json:"achievements"`
 }
 
 func sendMapItems(w http.ResponseWriter, r *http.Request) {
@@ -50,6 +51,7 @@ func sendMapItems(w http.ResponseWriter, r *http.Request) {
 		{
 			Id:          1,
 			Name:        "Palace",
+			Image:       "https://www.riu.com/es/binaris/piscina-riu-palace-las-americas-2016_tcm49-157129.jpg",
 			Description: "olololo",
 			Location: Location{
 				Id:  1,
@@ -75,6 +77,7 @@ func sendMapItems(w http.ResponseWriter, r *http.Request) {
 			Id:          1,
 			Name:        "Palace2",
 			Description: "Palace was builded in 1332.",
+			Image:       "https://taj.tajhotels.com/content/dam/luxury/hotels/Umaid_Bhavan/images/16x7/33397215.jpg",
 			Location: Location{
 				Id:  1,
 				Lat: 59.956333,
