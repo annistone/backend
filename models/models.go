930ff55c501331c15a48628d04003a4cbe06a42d
Ann Korepanova <annistonelivesp@gmail.com>
2017-10-19 11:17:27 +0000

    json changed by sqlite3 db; now only getMapItems api's supported

---
+++ models/models.go
@@ -0,0 +1,53 @@
+package models
+
+type InterfaceMapItem struct {
+	Id          int      `json:"id"`
+	Name        string   `json:"name"`
+	Description string   `json:"description"`
+	Image       string   `json:"image"`
+	Location    Location `json:"location"`
+	Link        Link     `json:"link"`
+}
+
+type Link struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+	Type int    `json:"type"`
+	Link string `json:"link"`
+}
+
+type Location struct {
+	Id  int     `json:"id"`
+	Lat float32 `json:"lat"`
+	Lng float32 `json:"lng"`
+}
+
+type MapItem struct {
+	Id          int      `json:"id"`
+	Name        string   `json:"name"`
+	Description string   `json:"description"`
+	Image       string   `json:"image"`
+	Location    Location `json:"location"`
+	Links       []Link   `json:"links"`
+}
+
+type Achievement struct {
+	Id          int    `json:"id"`
+	Text        string `json:"text"`
+	Description string `json:"description"`
+	Image       string `json:"image"`
+}
+
+type User struct {
+	Id           int           `json:"id"`
+	Name         string        `json:"name"`
+	Last_name    string        `json:"last_name"`
+	Rating       int           `json:"rating"`
+	Image        string        `json:"image"`
+	Achievements []Achievement `json:"achievements"`
+}
+
+type Prize struct {
+	Price       int         `json:"price"`
+	Achievement Achievement `json:"achievement"`
+}
